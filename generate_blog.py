6f13706983d202e06f8cf6f97122a201f068b3fa
droptonotify <droptonotify@icloud.com>
2025-07-06 14:04:09 +0600

    Update generate_blog.py

--- generate_blog.py
+++ generate_blog.py
@@ -1,3 +1,10 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import base64
+import os
+from google import genai
+from google.genai import types
 import os
 import re
 import requests
